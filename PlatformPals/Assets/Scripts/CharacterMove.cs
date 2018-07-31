18df855c7145be11cd44c816d8d90fd3730637cf
ChrisAttwood <christopher.attwood@outlook.com>
2018-07-31 17:11:12 +0100

    Initial Check in

---
+++ PlatformPals/Assets/Scripts/CharacterMove.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterMove : MonoBehaviour {
+
+    public int Speed = 10;
+
+    public Rigidbody2D Rigidbody2D;
+
+    private void Update()
+    {
+        float direction = Input.GetAxis("Horizontal");
+       
+        Rigidbody2D.AddForce(new Vector2(direction * Speed, 0f));
+    }
+}
